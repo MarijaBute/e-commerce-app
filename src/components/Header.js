3446afb19b63ab3782266f30781203970cd00572
Verica Mitreva <vericamitreva@Vericas-MacBook-Pro.local>
2023-08-28 19:30:18 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import logo from "../images/logo.svg"
+import cart from "../images/icon-cart.svg"
+import avatar from "../images/image-avatar.png"
+
+export default function Header () {
+   return (
+   <header className="site-header">
+    <div className="site-nav">
+        <img src={logo} alt="logo"/>
+        <ul>
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div> 
+    <div>
+        <img src={cart} alt="cart"/>
+        <img src={avatar} alt="avatar"/>
+    </div>
+    
+   </header>
+   )
+}
+
